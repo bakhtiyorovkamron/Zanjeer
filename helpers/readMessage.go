873504c099db9f5d7e07d4de979b2f0dd43bfd3b
HLIQKamron <bahtiyorovkamron8@gmail.com>
2024-03-26 16:30:41 +0500

    longitude fixed

--- helpers/readMessage.go
+++ helpers/readMessage.go
@@ -21,19 +21,19 @@ func ReadMessage(message, imei string) (models.Record, error) {
 	if err != nil {
 		return models.Record{}, err
 	}
-	altitude, err := strconv.ParseInt(message[54:58], 16, 64)
-	if err != nil {
-		return models.Record{}, err
-	}
+	// altitude, err := strconv.ParseInt(message[54:58], 16, 64)
+	// if err != nil {
+	// 	return models.Record{}, err
+	// }
 
-	angle, err := strconv.ParseInt(message[58:62], 16, 64)
-	if err != nil {
-		return models.Record{}, err
-	}
-	speed, err := strconv.ParseInt(message[64:68], 16, 64)
-	if err != nil {
-		return models.Record{}, err
-	}
+	// angle, err := strconv.ParseInt(message[58:62], 16, 64)
+	// if err != nil {
+	// 	return models.Record{}, err
+	// }
+	// speed, err := strconv.ParseInt(message[64:68], 16, 64)
+	// if err != nil {
+	// 	return models.Record{}, err
+	// }
 	size, err := StringToUint8(message)
 	if err != nil {
 		return models.Record{}, err
@@ -43,9 +43,9 @@ func ReadMessage(message, imei string) (models.Record, error) {
 		Imei:      imei,
 		Longitude: float64(longitude) * 0.0000001,
 		Latitude:  float64(latitude) * 0.0000001,
-		Altitude:  altitude,
-		Angle:     angle,
-		Speed:     speed,
-		Size:      uint8(size),
+		// Altitude:  altitude,
+		// Angle:     angle,
+		// Speed:     speed,
+		Size: uint8(size),
 	}, nil
 }
